a6c011d22eab7678bdb7495ec29e2f0ba92bebf1
Hilton Williams <hwilliams@luminoushilt.com>
2022-11-27 14:14:19 -0800

    First Commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./*.{html,js}", // "./src/**/*.{html,js}"
+    ],
+    theme: {
+        fontFamily: {
+            sans: ["Source Sans Pro", "sans-serif"],
+            serif: ["Raleway", "serif"],
+        },
+        extend: {},
+    },
+    plugins: [],
+};
